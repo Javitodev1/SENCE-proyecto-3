66cefa035c3314ef3046f14bf84adadef897a4ce
Javitodev <javito.dev@gmail.com>
2025-08-11 21:52:38 -0400

    cucumber init

---
+++ src/test/java/hooks/Hooks.java
@@ -0,0 +1,28 @@
+package hooks;
+
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import utils.DriverFactory;
+import io.cucumber.java.After;
+
+public class Hooks {
+    @Before
+    public void setUp() {
+        System.out.println("[HOOK] Abriendo navegador...");
+        WebDriverManager.chromedriver().setup();
+
+        DriverFactory.driver = new ChromeDriver();
+        DriverFactory.driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("[HOOK] Cerrando navegador...");
+        if (DriverFactory.driver != null) {
+            DriverFactory.driver.quit();
+        }
+    }
+}
